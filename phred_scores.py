39c953f7e26a9b143d5a29c6e25b4bb50be66925
degenerates <degenerates@derisilab105.ucsf.edu>
2018-09-26 16:27:23 -0700

    adding file to parse phred scores and test input

---
+++ phred_scores.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+
+
+# Author: Christina Stephens
+# Date: September 26, 2018
+
+"""
+    Except a fastq file and return a dictionary of the sequence id (key)
+    and list of P (probabilty of error) scores for the base pairs.
+
+    Calculate an average and standard deviation P for each read.
+
+    Plot a histogram of the average P for each read. Chech for trends. 
+"""
